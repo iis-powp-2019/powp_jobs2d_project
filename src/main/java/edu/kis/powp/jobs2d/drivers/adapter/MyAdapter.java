f7999a842b8cb5d25c28b63c189f941d7ee43f88
Czajkowski Mariusz <194930@edu.p.lodz.pl>
2019-04-02 11:49:59 +0200

    Symulacja wyświetlana jest już poprawnie w oknie aplikacji + poprawa rysowania

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,15 +4,17 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class MyAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	DrawPanelController drawPanelController;
 
 	public MyAdapter() {
-		super();
+		drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +29,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawPanelController.drawLine(line);
+		this.startX = x;
+		this.startY = y;
 	}
 
 	@Override
