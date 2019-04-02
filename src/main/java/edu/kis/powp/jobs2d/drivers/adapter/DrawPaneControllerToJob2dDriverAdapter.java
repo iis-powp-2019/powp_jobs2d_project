a80bbadc5ce3d9cd0b8ca22780937b8caa553429
astepaniuk <truemob2@gmail.com>
2019-04-02 11:45:43 +0200

    Adapter renamed + DrawPaneController is injected instead of inherited

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPaneControllerToJob2dDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPaneControllerToJob2dDriverAdapter.java
@@ -8,11 +8,13 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawPaneControllerToJob2dDriverAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController controller;
 
-	public MyAdapter() {
+	public DrawPaneControllerToJob2dDriverAdapter(DrawPanelController controller) {
 		super();
+		this.controller = controller;
 	}
 
 	@Override
@@ -28,7 +30,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setEndCoordinates(x, y);
 		this.startX = x;
 		this.startY = y;
-		drawLine(line);
+		controller.drawLine(line);
 	}
 
 	@Override
