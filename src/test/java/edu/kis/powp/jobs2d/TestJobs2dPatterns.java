85edd8778774b3054638eccc585dd486ce9a14f8
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-04-08 20:41:51 +0200

    3.3.1-2 added specialLineAdapter and test possibility for testing it

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,6 +10,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -46,7 +47,10 @@ public class TestJobs2dPatterns {
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
 		Job2dDriver testDriver = new DriverAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		DriverFeature.addDriver("Solid line Simulator", testDriver);
+
+		Job2dDriver lineDrawerAdapter = new LineDrawerAdapter();
+		DriverFeature.addDriver("Special line Simulator", lineDrawerAdapter);
 
 		DriverFeature.updateDriverInfo();
 	}
