45bdb7167ac0b61f9e46b7f59b9104ffe10f2037
dettlaffb <209295@edu.p.lodz.pl>
2019-04-10 15:40:07 +0200

    3.2.1 Dependency Injection and Renaming Adapter class

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingJob2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingJob2dAdapter.java
@@ -4,15 +4,18 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawingJob2dAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+    private DrawPanelController drawPanelController;
 
-	public MyAdapter() {
+	public DrawingJob2dAdapter(DrawPanelController drawPanelController) {
 		super();
+		this.drawPanelController = drawPanelController;
 	}
 
 	@Override
@@ -26,7 +29,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		drawLine(line);
+		drawPanelController.drawLine(line);
 
 		setPosition(x,y);
 	}
