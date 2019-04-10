23a0706b11b7f3da8212bedaf4827013ace54031
Lukkario <michallukianski.0@gmail.com>
2019-04-10 15:14:56 +0200

    3.3.1 Added implementation of LineDrawerAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,70 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+
+public class LineDrawerAdapter implements Job2dDriver {
+    private int startX = 0, startY = 0;
+    ILine line;
+    LineType type;
+
+    public LineDrawerAdapter(){
+        super();
+        line = LineFactory.getBasicLine();
+    }
+
+
+
+    public LineDrawerAdapter(LineType type) {
+        super();
+        if(type == LineType.NORMAL)
+        {
+            this.line = LineFactory.getBasicLine();
+        }
+        else if(type == LineType.DOTTED)
+        {
+            this.line = LineFactory.getDottedLine();
+        }
+        else if(type == LineType.SPECIAL)
+        {
+            this.line = LineFactory.getSpecialLine();
+        }
+
+        this.type = type;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        DrawerFeature.getDrawerController().drawLine(line);
+        setPosition(x,y);
+    }
+
+    @Override
+    public String toString() {
+        if(this.type == LineType.NORMAL)
+            return "Normal line drawer";
+        if(this.type == LineType.DOTTED)
+            return "Dotted line drawer";
+        if(this.type == LineType.SPECIAL)
+            return "Special line drawer";
+        return "Unknown line drawer";
+    }
+
+    public enum LineType
+    {
+        NORMAL,
+        DOTTED,
+        SPECIAL
+    }
+}
