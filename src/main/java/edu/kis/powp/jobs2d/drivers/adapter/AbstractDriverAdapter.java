70e73e14b52893650719999004f232835f0e850d
pawel10x <x10player10x@gmail.com>
2019-04-12 01:00:20 +0200

    added AbstractDriverAdapter 3.4.1

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -0,0 +1,22 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+public class AbstractDriverAdapter extends AbstractDriver {
+
+	public AbstractDriverAdapter() {
+		super(0, 0);
+	}
+	
+	@Override public void operateTo(int x, int y) {
+        ILine line = LineFactory.getBasicLine();
+        line.setStartCoordinates(this.getX(), this.getY());
+        line.setEndCoordinates(x, y);
+
+        DrawerFeature.getDrawerController().drawLine(line);
+        setPosition(x,y);
+    }
+}
