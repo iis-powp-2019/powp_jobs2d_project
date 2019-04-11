25d3aae88a9c2b7964c06e27bf5f35ef22b2eab8
skydriver95 <prostreet950@gmail.com>
2019-04-11 15:30:29 +0200

    3.2. Naprawiono wyświelanie figury w odpowiednim oknie i jej kształt

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -5,14 +5,17 @@ import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+
 /**
  * driver adapter to drawer with several bugs.
  */
 public class MyAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController drawPanelController;
 
-	public MyAdapter() {
+	public MyAdapter(DrawPanelController drawPanelController) {
 		super();
+		this.drawPanelController = drawPanelController;
 	}
 
 	@Override
@@ -27,7 +30,8 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		this.drawPanelController.drawLine(line);
+		setPosition(x,y);
 	}
 
 	@Override
