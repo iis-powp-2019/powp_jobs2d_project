cf6736b4e45698f255a4a775290d511f8fd48578
Natalia Ciosek <n_ciosek@wp.pl>
2019-04-11 15:27:22 +0200

    3.2 zmiana nazwy MyAdapter na Job2dDriverAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
@@ -4,17 +4,16 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class Job2dDriverAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	private DrawPanelController drawPanelController;
 
-	public MyAdapter(DrawPanelController drawPanelController) {
+	public Job2dDriverAdapter(DrawPanelController drawPanelController) {
 		super();
 		this.drawPanelController = drawPanelController;
 	}
