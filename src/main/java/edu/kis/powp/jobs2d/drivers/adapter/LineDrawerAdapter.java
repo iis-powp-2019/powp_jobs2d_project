3320638e70269507ba4fa19071a9833741040993
Lucat0n <lukasz662@gmail.com>
2019-04-11 16:08:55 +0200

    3.3.1 Implementacja klasy LineDrawerAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,32 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+public class LineDrawerAdapter implements Job2dDriver{
+
+    private int startX = 0;
+    private int startY = 0;
+    private ILine line;
+
+    public LineDrawerAdapter(){
+        super();
+        line = LineFactory.getBasicLine();
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        setPosition(x,y);
+        DrawerFeature.getDrawerController().drawLine(line);
+    }
+}
