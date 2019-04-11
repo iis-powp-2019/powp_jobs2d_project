ae84efd6de676eecb44131a4fefca179b92e7cbf
Pawel Wolski <209509@CARBON.CTI.P.LODZ.PL>
2019-04-11 15:09:01 +0200

    3.1 - Rysowanie w oknie glownym

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,34 +4,37 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class MyAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
-		super();
-	}
+    private int startX = 0, startY = 0;
 
-	@Override
-	public void setPosition(int x, int y) {
-		this.startX = x;
-		this.startY = y;
-	}
+    public MyAdapter() {
+        super();
+    }
 
-	@Override
-	public void operateTo(int x, int y) {
-		ILine line = LineFactory.getBasicLine();
-		line.setStartCoordinates(this.startX, this.startY);
-		line.setEndCoordinates(x, y);
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
 
-		drawLine(line);
-	}
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getBasicLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
 
-	@Override
-	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
-	}
+        DrawerFeature.getDrawerController()
+                     .drawLine(line);
+    }
+
+    @Override
+    public String toString() {
+        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+    }
 }
