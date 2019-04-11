00cbdcc45f81fecbeb6c27bc8224cdc417c37a47
Lukkario <michallukianski.0@gmail.com>
2019-04-11 08:38:50 +0200

    3.3.3 added EnhancedBasicLine

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/factory/EnhancedLine/EnhancedBasicLine.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.jobs2d.drivers.factory.EnhancedLine;
+
+import java.awt.*;
+
+public class EnhancedBasicLine extends AbstractEnhancedLine {
+    public EnhancedBasicLine()
+    {
+        super();
+        color = Color.BLACK;
+        thickness = 3.0f;
+        dotted = false;
+    }
+}
