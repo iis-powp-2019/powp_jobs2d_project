18f08b205d9436659c6a8612eca12096fd167020
Igor Jarek <201953@edu.p.lodz.pl>
2019-04-16 00:38:47 +0200

    Lab 3 - 3.3.1 and 3.3.2

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.Job2d2DrawerAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectFiguresJoeOneOptionListener;
 import edu.kis.powp.jobs2d.events.SelectFiguresJoeTwoOptionListener;
@@ -43,6 +44,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new Job2d2DrawerAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver specialLineDriver = new LineDrawerAdapter();
+		DriverFeature.addDriver("Special Line Simulator", specialLineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
