a4567a94b8ebcbcb11ef03d61382484ae9c81c9b
201953 <201953@edu.p.lodz.pl>
2019-04-16 11:03:19 +0200

    Lab 4 - 3.1.1

---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+
+    private int x, y;
+    private Job2dDriver driver;
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void execute() {
+        driver.setPosition(x, y);
+    }
+}
