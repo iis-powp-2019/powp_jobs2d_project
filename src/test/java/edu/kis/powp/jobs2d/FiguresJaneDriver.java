a5933874c342c1e79317724ba58221386bb20d15
banan <szy.grzelak@gmail.com>
2019-04-18 00:08:07 +0200

    3.4.1 Dodanie klasy FiguresJaneDriver

---
+++ src/test/java/edu/kis/powp/jobs2d/FiguresJaneDriver.java
@@ -0,0 +1,28 @@
+package edu.kis.powp.jobs2d;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+public class FiguresJaneDriver extends AbstractDriver {
+
+    private ILine line;
+
+    public FiguresJaneDriver(int x, int y, ILine line) {
+        super(x, y);
+        this.line = line;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        line.setStartCoordinates(this.getX(), this.getY());
+        line.setEndCoordinates(x, y);
+        setPosition(x, y);
+        DrawerFeature.getDrawerController()
+                     .drawLine(line);
+    }
+
+    @Override
+    public String toString() {
+        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+    }
+}
