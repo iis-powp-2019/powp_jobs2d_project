bfebe5ffde9af2d3eaf61603b493c5e4546ba79e
struszczak.kamila <209472@edu.p.lodz.pl>
2019-04-17 22:30:47 +0200

    dodanie możliwości narysowania drugiego wzorca

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -5,12 +5,11 @@ import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawPanelControllerToJob2dDriverAdapter;
-import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
-import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
+import edu.kis.powp.jobs2d.drivers.adapter.SpecialLineDrawerAdapter;
+import edu.kis.powp.jobs2d.events.SelectTestFigureOption1Listener;
+import edu.kis.powp.jobs2d.events.SelectTestFigureOption2Listener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
@@ -23,10 +22,15 @@ public class TestJobs2dPatterns {
 	 * @param application Application context.
 	 */
 	private static void setupPresetTests(Application application) {
-		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
+
+		SelectTestFigureOption1Listener figure1 = new SelectTestFigureOption1Listener(
 				DriverFeature.getDriverManager());
 
-		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		SelectTestFigureOption2Listener figure2 = new SelectTestFigureOption2Listener(
+				DriverFeature.getDriverManager());
+
+		application.addTest("Figure Joe 1", figure1);
+		application.addTest("Figure Joe 2", figure2);
 	}
 
 	/**
---
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOption2Listener.java
@@ -0,0 +1,22 @@
+package edu.kis.powp.jobs2d.events;
+
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectTestFigureOption2Listener implements ActionListener {
+
+    private DriverManager driverManager;
+
+    public SelectTestFigureOption2Listener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+    }
+
+}
