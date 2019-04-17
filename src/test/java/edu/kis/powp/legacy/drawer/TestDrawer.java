fbfd0368c97a6d5b7e0f9c86c896f0247804f8e6
Monika Odartus <209410@edu.p.lodz.pl>
2019-04-17 22:34:43 +0200

    3.2.2 test na wzorcu figureScript2

--- src/test/java/edu/kis/powp/legacy/drawer/TestDrawer.java
+++ src/test/java/edu/kis/powp/legacy/drawer/TestDrawer.java
@@ -4,6 +4,9 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.drivers.adapter.MyAdapter;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 /**
  * Drawer test.
@@ -15,11 +18,16 @@ public class TestDrawer {
 	 * Drawer test.
 	 */
 	public static void main(String[] args) {
-		DrawPanelController controller = new DrawPanelController();
-		DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
+		Application app = new Application("Drawer App");
+		MyAdapter controller = new MyAdapter(app);
+		FiguresJoe.figureScript1(controller);
+		app.setVisibility(true);
+		
+		
+		/*DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(-100, -60);
 		line.setEndCoordinates(60, 130);
-		controller.drawLine(line);
+		controller.drawLine(line);*/
 	}
 }
