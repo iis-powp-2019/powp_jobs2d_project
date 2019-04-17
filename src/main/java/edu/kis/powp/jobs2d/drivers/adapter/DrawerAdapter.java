428635806440fd77ff75e49b6ba6e51e8f99166a
r.witczyk <r.witczyk@wp.pl>
2019-04-17 02:20:49 +0200

    3.2.1 One window and more suitable name for adapter class

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -4,14 +4,16 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	DrawPanelController drawerFeature = new DrawerFeature().getDrawerController();
 
-	public MyAdapter() {
+	public DrawerAdapter() {
 		super();
 	}
 
@@ -26,10 +28,10 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		this.startX=x;
-		this.startY=y;
 
-		drawLine(line);
+		drawerFeature.drawLine(line);
+		setPosition(x,y);
+
 	}
 
 	@Override
