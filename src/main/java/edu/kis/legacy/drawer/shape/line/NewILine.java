3ff104199e8b594434e1e88e1bf67a214507e582
Monika Odartus <209410@edu.p.lodz.pl>
2019-04-18 00:23:32 +0200

    3.3.3 implementacja doboru parametrow linii

---
+++ src/main/java/edu/kis/legacy/drawer/shape/line/NewILine.java
@@ -0,0 +1,13 @@
+package edu.kis.legacy.drawer.shape.line;
+
+import java.awt.Color;
+
+public class NewILine extends AbstractLine{
+	
+	public NewILine(Color color, float thickness, boolean dotted) {
+        super();
+        this.color = color;
+        this.thickness = thickness;
+        this.dotted = dotted;
+    }
+}
