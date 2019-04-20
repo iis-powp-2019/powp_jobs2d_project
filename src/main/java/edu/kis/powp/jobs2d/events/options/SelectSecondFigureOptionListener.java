03bf3644530c49f1328843f5625d7ace89d8c988
dettlaffb <209295@edu.p.lodz.pl>
2019-04-20 12:43:53 +0200

    3.2.3 Created two classes for Joe's Figures

---
+++ src/main/java/edu/kis/powp/jobs2d/events/options/SelectSecondFigureOptionListener.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.events.options;
+
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectSecondFigureOptionListener implements ActionListener {
+
+    private DriverManager driverManager;
+
+    public SelectSecondFigureOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+    }
+}
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,7 +10,8 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawingJob2dAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
-import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
+import edu.kis.powp.jobs2d.events.options.SelectFirstFigureOptionListener;
+import edu.kis.powp.jobs2d.events.options.SelectSecondFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
@@ -23,10 +24,13 @@ public class TestJobs2dPatterns {
 	 * @param application Application context.
 	 */
 	private static void setupPresetTests(Application application) {
-		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
+		SelectFirstFigureOptionListener selectFirstFigureOptionListener = new SelectFirstFigureOptionListener(
+				DriverFeature.getDriverManager());
+		SelectSecondFigureOptionListener selectSecondFigureOptionListener = new SelectSecondFigureOptionListener(
 				DriverFeature.getDriverManager());
 
-		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		application.addTest("Figure Joe 1", selectFirstFigureOptionListener);
+		application.addTest("Figure Joe 2", selectSecondFigureOptionListener);
 	}
 
 	/**
