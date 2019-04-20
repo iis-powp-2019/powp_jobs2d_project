8747e5f5f472d65993ef6ee876883c6da8472252
dettlaffb <209295@edu.p.lodz.pl>
2019-04-20 12:50:26 +0200

    3.3.1 Little refactor in Adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingJob2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingJob2dAdapter.java
@@ -9,32 +9,31 @@ import edu.kis.powp.jobs2d.Job2dDriver;
  * driver adapter to drawer with several bugs.
  */
 public class DrawingJob2dAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;
+
+    private int startX = 0;
+	private int startY = 0;
+
     private DrawPanelController drawPanelController;
 
-	public DrawingJob2dAdapter(DrawPanelController drawPanelController) {
-		super();
-		this.drawPanelController = drawPanelController;
-	}
+    public DrawingJob2dAdapter(DrawPanelController drawPanelController) {
+        super();
+        this.drawPanelController = drawPanelController;
+    }
 
-	@Override
-	public void setPosition(int x, int y) {
-		this.startX = x;
-		this.startY = y;
-	}
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
 
-	@Override
-	public void operateTo(int x, int y) {
-		ILine line = LineFactory.getBasicLine();
-		line.setStartCoordinates(this.startX, this.startY);
-		line.setEndCoordinates(x, y);
-		drawPanelController.drawLine(line);
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getSpecialLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
 
-		setPosition(x,y);
-	}
-
-	@Override
-	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
-	}
+        drawPanelController.drawLine(line);
+        
+        setPosition(x, y);
+    }
 }
