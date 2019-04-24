60cef1a6723485c389de5f692009f10179c42303
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-04-24 15:04:25 +0200

    3.2.3-Refactor

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -20,13 +20,13 @@ public class TestJobs2dPatterns {
 	/**
 	 * Setup test concerning preset figures in context.
 	 * 
-	 * @param application Application context.
+	 * @param application
+	 *            Application context.
 	 */
 	private static void setupPresetTests(Application application) {
 		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
 				DriverFeature.getDriverManager());
 
-		
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
 		application.addTest("Figure Joe 2", selectTestFigureOptionListener);
 	}
@@ -34,7 +34,8 @@ public class TestJobs2dPatterns {
 	/**
 	 * Setup driver manager, and set default driver for application.
 	 * 
-	 * @param application Application context.
+	 * @param application
+	 *            Application context.
 	 */
 	private static void setupDrivers(Application application) {
 		Job2dDriver loggerDriver = new LoggerDriver();
@@ -50,7 +51,8 @@ public class TestJobs2dPatterns {
 	/**
 	 * Auxiliary routines to enable using Buggy Simulator.
 	 * 
-	 * @param application Application context.
+	 * @param application
+	 *            Application context.
 	 */
 	private static void setupDefaultDrawerVisibilityManagement(Application application) {
 		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
@@ -62,7 +64,8 @@ public class TestJobs2dPatterns {
 	/**
 	 * Setup menu for adjusting logging settings.
 	 * 
-	 * @param application Application context.
+	 * @param application
+	 *            Application context.
 	 */
 	private static void setupLogger(Application application) {
 		application.addComponentMenu(Logger.class, "Logger", 0);
