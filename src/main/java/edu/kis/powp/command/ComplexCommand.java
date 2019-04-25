28c11be6581dc676adfdcda017af9296b4a322ec
209410 <209410@CARBON.CTI.P.LODZ.PL>
2019-04-25 15:11:07 +0200

    3.1.3 - klasa ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,26 @@
+package edu.kis.powp.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ComplexCommand implements DriverCommand{
+
+	private List<DriverCommand> driverCommandList;
+	
+	public ComplexCommand() {
+		driverCommandList = new ArrayList<DriverCommand>();
+	}
+	
+	public void addToDriverCommandList(DriverCommand command) {
+		driverCommandList.add(command);
+	}
+
+	@Override
+	public void execute(Job2dDriver j2dd) {
+		for(DriverCommand dc : driverCommandList) {
+			dc.execute(j2dd);
+		}
+	}
+}
