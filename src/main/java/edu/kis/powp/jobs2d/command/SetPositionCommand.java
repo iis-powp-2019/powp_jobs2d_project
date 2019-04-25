1bd8e40a24a77d8204325429c880e7807ab2126b
Pawel Wolski <209509@carbon.cti.p.lodz.pl>
2019-04-25 14:32:56 +0200

    3.1.1 - Add classes and intefrace in realtion with UML diagram

---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.jobs2d.command;
+
+public class SetPositionCommand implements DriverCommand {
+
+    public int x;
+    public int y;
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+
+    }
+}
