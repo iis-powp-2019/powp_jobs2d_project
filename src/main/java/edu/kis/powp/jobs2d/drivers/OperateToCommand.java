ea1bed87d0618b9045ace5af7daa5ce3b5f6c2c1
209489 <lukasztracz195@gmail.com>
2019-04-25 14:45:13 +0200

    3.1.2 Zaimplementowanie class hierarchii

--- src/main/java/edu/kis/powp/jobs2d/drivers/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/OperateToCommand.java
@@ -1,8 +1,20 @@
 package edu.kis.powp.jobs2d.drivers;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute() {
-
+        DriverManager driverManager = new DriverManager();
+        Job2dDriver jobs2dDriver = driverManager.getCurrentDriver();
+        jobs2dDriver.operateTo(x, y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SetPositionCommand.java
@@ -1,8 +1,20 @@
 package edu.kis.powp.jobs2d.drivers;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute() {
-        
+        DriverManager driverManager = new DriverManager();
+        Job2dDriver jobs2dDriver = driverManager.getCurrentDriver();
+        jobs2dDriver.setPosition(x, y);
     }
 }
