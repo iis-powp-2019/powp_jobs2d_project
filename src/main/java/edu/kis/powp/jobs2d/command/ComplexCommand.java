3a57d189253f0a40b53f7a24d6d7d97830767eaa
Pawel Wolski <209509@CARBON.CTI.P.LODZ.PL>
2019-04-25 15:35:41 +0200

    3.1.2.Test - Test for command figure joe 3 - Rectangle

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,22 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+
+    private List<DriverCommand> commands = new ArrayList();
+
+    public void addCommand(DriverCommand driverCommand) {
+        commands.add(driverCommand);
+    }
+
+    @Override
+    public void execute() {
+        for (int i = 0; i < commands.size(); i++) {
+            DriverCommand e = commands.get(i);
+            e.execute();
+        }
+    }
+
+}
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener3.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener3.java
@@ -2,14 +2,10 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
-import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.ComplexCommand;
 import edu.kis.powp.jobs2d.command.OperateToCommand;
-import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
-import edu.kis.powp.jobs2d.drivers.adapter.DrawerAdapter;
 
 public class SelectTestFigureOptionListener3 implements ActionListener {
 
@@ -21,12 +17,12 @@ public class SelectTestFigureOptionListener3 implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        List<DriverCommand> commands = new ArrayList();
-        commands.add(new SetPositionCommand(new DrawerAdapter(), 3, 5));
-        SetPositionCommand command1 = (SetPositionCommand) commands.get(0);
-        command1.execute();
-        commands.add(new OperateToCommand(new DrawerAdapter(), 4, 8));
-        OperateToCommand command2 = (OperateToCommand) commands.get(1);
-        command2.execute();
+        ComplexCommand complexCommand = new ComplexCommand();
+        complexCommand.addCommand(new OperateToCommand(driverManager.getCurrentDriver(), 0, 100));
+        complexCommand.addCommand(new OperateToCommand(driverManager.getCurrentDriver(), 200, 100));
+        complexCommand.addCommand(new OperateToCommand(driverManager.getCurrentDriver(), 200, 0));
+        complexCommand.addCommand(new OperateToCommand(driverManager.getCurrentDriver(), 0, 0));
+
+        complexCommand.execute();
     }
 }
