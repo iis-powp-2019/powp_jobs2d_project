85a7f5cd9c68ece9c4ffc870a6583b6d23bae4cd
209360 <209360@10.0.75.1>
2019-04-25 15:36:52 +0200

    3.1.3 added ComplexCommand

--- src/test/java/edu/kis/powp/jobs2d/events/SelectCommandTestOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectCommandTestOptionListener.java
@@ -3,11 +3,10 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.command.ComplexCommand;
 import edu.kis.powp.command.DriverCommand;
 import edu.kis.powp.command.OperateToCommand;
-import edu.kis.powp.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
-import edu.kis.powp.jobs2d.drivers.adapter.DrawerAdapter;
 
 public class SelectCommandTestOptionListener implements ActionListener {
 
@@ -19,11 +18,17 @@ public class SelectCommandTestOptionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        DriverCommand driverCommand = new OperateToCommand(new DrawerAdapter(), 3, 20);
-        driverCommand.execute();
-        DriverCommand driverCommand2 = new SetPositionCommand(new DrawerAdapter(), 3, 20);
-        driverCommand2.execute();
-        DriverCommand driverCommand3 = new OperateToCommand(new DrawerAdapter(), 10, 5);
-        driverCommand3.execute();
+        ComplexCommand complex = new ComplexCommand();
+
+        DriverCommand driverCommand = new OperateToCommand(driverManager.getCurrentDriver(), 0, 50);
+        complex.addCommand(driverCommand);
+        DriverCommand driverCommand2 = new OperateToCommand(driverManager.getCurrentDriver(), 50, 50);
+        complex.addCommand(driverCommand2);
+        DriverCommand driverCommand4 = new OperateToCommand(driverManager.getCurrentDriver(), 50, 0);
+        complex.addCommand(driverCommand4);
+        DriverCommand driverCommand5 = new OperateToCommand(driverManager.getCurrentDriver(), 0, 0);
+        complex.addCommand(driverCommand5);
+
+        complex.execute();
     }
 }
