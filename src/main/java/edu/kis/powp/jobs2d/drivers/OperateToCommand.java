365fc9cddb3e7493908125f29ba63383bf6223d8
209489 <lukasztracz195@gmail.com>
2019-04-25 14:32:56 +0200

    3.1.1 Utworzenie hierarchii class Command design Patern

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/OperateToCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.drivers;
+
+public class OperateToCommand implements DriverCommand {
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SetPositionCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.drivers;
+
+public class SetPositionCommand implements DriverCommand {
+    @Override
+    public void execute() {
+        
+    }
+}
