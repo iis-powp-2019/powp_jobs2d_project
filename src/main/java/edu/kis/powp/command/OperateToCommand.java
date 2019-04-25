1ff49434a6ce6a4e8233911fe9bbc99593056192
Natalia Ciosek <n_ciosek@wp.pl>
2019-04-25 15:34:23 +0200

    3.1.2. zmiany w implementacji klas hierarchii

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -5,13 +5,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class OperateToCommand implements DriverCommand {
 
     private int x, y;
+    private Job2dDriver job2dDriver;
 
     OperateToCommand(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    @Override public void execute(Job2dDriver job2dDriver) {
+    @Override public void execute() {
         job2dDriver.operateTo(x,y);
     }
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -5,13 +5,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class SetPositionCommand implements DriverCommand {
 
    private int x, y;
+   private Job2dDriver job2dDriver;
 
    SetPositionCommand(int x, int y){
        this.x = x;
        this.y = y;
    }
 
-    @Override public void execute(Job2dDriver job2dDriver) {
+    @Override public void execute() {
        job2dDriver.setPosition(x, y);
     }
 
