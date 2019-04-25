effc986fb5e2ccdae9cc32d0e1df0c1fe780990e
Natalia Ciosek <n_ciosek@wp.pl>
2019-04-25 15:35:11 +0200

    3.1.3. zaimplementowanie klasy ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.command;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+
+    private List<DriverCommand> listOfCommand;
+
+    public ComplexCommand(List<DriverCommand> list) {
+        listOfCommand = list;
+    }
+
+    @Override public void execute() {
+        for (DriverCommand item : listOfCommand) {
+            item.execute();
+        }
+    }
+}
