211a605b882270814368aa252f8b4f3c8223f09e
209322 <209322@CARBON.CTI.P.LODZ.PL>
2019-04-25 14:51:02 +0200

    3.1.2 Implementacja klas SetPositionCommand, OperateToCommand

--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -1,12 +1,19 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 
 	private int x;
 	private int y;
 
+	SetPositionCommand(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	@Override
-	public void execute() {
-
+	public void execute(Job2dDriver object) {
+		object.setPosition(x,y);
 	}
 }
