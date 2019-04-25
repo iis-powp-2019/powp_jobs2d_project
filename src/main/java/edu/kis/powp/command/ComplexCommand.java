35e15e60160fd8e185d326d724992b5fe156fcc8
209424 <209424@edu.p.lodz.pl>
2019-04-25 15:39:35 +0200

    Command 3.1.3 - utworzono klasę ComplexCommand i dodano do niej testy w Job2dDriverTest

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,17 @@
+package edu.kis.powp.command;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+
+    private List<DriverCommand> driverCommands;
+
+    public ComplexCommand(List<DriverCommand> driverCommands) {
+        this.driverCommands = driverCommands;
+    }
+
+    @Override
+    public void execute() {
+        driverCommands.forEach(DriverCommand::execute);
+    }
+}
--- src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
+++ src/test/java/edu/kis/powp/jobs2d/Job2dDriverTest.java
@@ -1,7 +1,14 @@
 package edu.kis.powp.jobs2d;
 
+import edu.kis.powp.command.ComplexCommand;
+import edu.kis.powp.command.DriverCommand;
+import edu.kis.powp.command.OperateToCommand;
+import edu.kis.powp.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Plotter test.
@@ -15,7 +22,24 @@ public class Job2dDriverTest {
 	 * Driver test.
 	 */
 	public static void main(String[] args) {
+		List<DriverCommand> driverCommands = new ArrayList<>();
+		ComplexCommand complexCommand;
+
+		driverCommands.add(new SetPositionCommand(-120, -120, driver));
+		driverCommands.add(new OperateToCommand(120, -120, driver));
+		driverCommands.add(new OperateToCommand(120, 120, driver));
+		driverCommands.add(new OperateToCommand(-120, -120, driver));
+		driverCommands.add(new OperateToCommand(-120, -120, driver));
+		driverCommands.add(new OperateToCommand(120, 120, driver));
+		driverCommands.add(new SetPositionCommand(120, -120, driver));
+		driverCommands.add(new OperateToCommand(-120, 120, driver));
+
+		complexCommand = new ComplexCommand(driverCommands);
+		complexCommand.execute();
+
+		System.out.println();
 		FiguresJoe.figureScript1(driver);
+		System.out.println();
 		FiguresJoe.figureScript2(driver);
 	}
 
@@ -30,5 +54,5 @@ public class Job2dDriverTest {
 		public void setPosition(int x, int y) {
 			System.out.println("Driver setPosition action...");
 		}
-	};
+	}
 }
