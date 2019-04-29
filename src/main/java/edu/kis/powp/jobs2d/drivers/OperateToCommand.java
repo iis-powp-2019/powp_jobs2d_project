160e756de0123ab45d911ab3096e169999bd6bfa
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-04-29 14:48:27 +0200

    (Command) 3.1.1 Hierarchia klas

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/OperateToCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.drivers;
+
+public class OperateToCommand implements DriverCommand {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SetPositionCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.drivers;
+
+public class SetPositionCommand implements DriverCommand{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
