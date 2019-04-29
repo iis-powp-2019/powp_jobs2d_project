172a3c6f63ed3c6e6502d7850a6b7a679cf27c29
Wojciech Oset <209415@edu.p.lodz.pl>
2019-04-29 21:47:30 +0200

    3.4.2 - Adding FigureJane tests

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,11 +10,14 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawJobs2dAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.AbstractDriverAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionOneListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionTwoListener;
+import edu.kis.powp.jobs2d.events.SelectTestFigureJaneOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+
 
 public class TestJobs2dPatterns {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -29,9 +32,14 @@ public class TestJobs2dPatterns {
 				DriverFeature.getDriverManager());
 		SelectTestFigureOptionTwoListener selectTestFigureOptionTwoListener = new SelectTestFigureOptionTwoListener(
 				DriverFeature.getDriverManager());
+		SelectTestFigureJaneOptionListener selectTestFigureJaneOptionListener = new SelectTestFigureJaneOptionListener(
+				DriverFeature.getDriverManager());
+
+
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionOneListener);
 		application.addTest("Figure Joe 2", selectTestFigureOptionTwoListener);
+		application.addTest("Figure Jane", selectTestFigureJaneOptionListener);
 	}
 
 	/**
@@ -49,6 +57,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testSpecialDriver = new LineDrawerAdapter(DrawerFeature.getDrawerController());
 		DriverFeature.addDriver("Special Simulator", testSpecialDriver);
+
+		Job2dDriver testAbstractDriver = new AbstractDriverAdapter(DrawerFeature.getDrawerController());
+		DriverFeature.addDriver("Jane Simulator", testAbstractDriver);
 
 
 		DriverFeature.updateDriverInfo();
