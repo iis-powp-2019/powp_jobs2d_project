8db3348922c7b0ae1e921c86d2cc604842c3b201
Adam Sylla <sylla.adam.97@gmail.com>
2019-04-29 14:48:11 +0200

    3.1.1 *added and implemented proper classes and interfaces

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,22 +1,21 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
 
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    OperateToCommand(Job2dDriver driver, int x, int y){
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
 
     @Override
     public void execute() {
-
-    }
-
-    @Override
-    public void setPosition(int i, int i1) {
-
-    }
-
-    @Override
-    public void operateTo(int i, int i1) {
-
+        driver.operateTo(x,y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -1,22 +1,22 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    SetPositionCommand(Job2dDriver driver, int x, int y){
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
 
     @Override
     public void execute() {
-
+        driver.setPosition(x,y);
     }
 
-    @Override
-    public void setPosition(int i, int i1) {
-
-    }
-
-    @Override
-    public void operateTo(int i, int i1) {
-
-    }
 }
