9c0557af246bc8c2014d32558b3c99dceec03854
Zandros229 <ernest.figura548@gmail.com>
2019-04-29 14:33:18 +0200

    Command 3.1.1 add SetPositionCommand class

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/DriverCommand.java
@@ -1,4 +1,7 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
-public interface DriverCommand {
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public interface DriverCommand extends Job2dDriver {
+    public void execute();
 }
