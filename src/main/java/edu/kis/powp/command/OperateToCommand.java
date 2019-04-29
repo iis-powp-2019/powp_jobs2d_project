318bd9f047cc341e8a8a373f74b67db943423d11
Leherguargin <maciejleszczynski1997@gmail.com>
2019-04-29 02:09:43 +0200

    3.1.2

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -4,16 +4,18 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class OperateToCommand implements DriverCommand{
 
+    Job2dDriver job2dDriver;
     private int x;
     private int y;
 
-    OperateToCommand(int x, int y){
+    OperateToCommand(Job2dDriver job2dDriver, int x, int y){
         this.x = x;
         this.y = y;
+        this.job2dDriver = job2dDriver;
     }
 
     @Override
-    public void execute(Job2dDriver job2dDriver) {
+    public void execute() {
         job2dDriver.operateTo(x, y);
     }
 }
