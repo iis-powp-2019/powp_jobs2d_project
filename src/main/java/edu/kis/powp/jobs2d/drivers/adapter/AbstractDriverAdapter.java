3cabef93a9d818f92b083a003795a681efdd1697
209278 <209278@edu.p.lodz.pl>
2019-05-01 13:57:28 +0200

    2.4.1 AbstractDriverAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -0,0 +1,35 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+public class AbstractDriverAdapter extends AbstractDriver {
+
+    private int startX = 0, startY = 0;
+    private DrawPanelController drawPanelController;
+
+    public AbstractDriverAdapter(int x, int y) {
+        super(x, y);
+        this.drawPanelController = DrawerFeature.getDrawerController();
+    }
+
+    public AbstractDriverAdapter(int x, int y, DrawPanelController drawPanelController) {
+        super(x, y);
+        this.drawPanelController = drawPanelController;
+    }
+
+
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getBasicLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+
+        drawPanelController.drawLine(line);
+
+        setPosition(x, y);
+    }
+}
