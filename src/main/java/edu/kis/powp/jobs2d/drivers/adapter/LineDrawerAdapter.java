8f55d94d066dbfec250e956bb51f8ca1fb10940e
Delicja05 <dewicusia@op.pl>
2019-05-01 15:53:01 +0200

    3.3.1. - implementacja LineDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -1,20 +1,19 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
-import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;	
-	private DrawPanelController drawPanelController;
+public class LineDrawerAdapter implements Job2dDriver {
+	private int startX = 0, startY = 0;
+	private ILine line;
 
-	public Job2dAdapter(DrawPanelController drawPanelController) {
+	public LineDrawerAdapter(ILine line) {
 		super();
-		this.drawPanelController = drawPanelController;
+		this.line = line;
 	}
 
 	@Override
@@ -25,16 +24,15 @@ public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public void operateTo(int x, int y) {
-		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
 		setPosition(x,y);
-		drawPanelController.drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
 	public String toString() {
-		return "Job2dAdapter";
+		return "LineDrawerAdapter";
 	}
 }
