5c89fef8b2886320c8a17239cd64a477e723f652
wsobczyk <wiktoriasobczyk@gmail.com>
2019-05-06 00:07:40 +0200

    3.3.1 - bugfix changes in LineDraweAdapter implementation

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -1,14 +1,28 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
+import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 
-public class LineDrawerAdapter implements Job2dDriver {
+public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
 
     int startX = 0;
     int startY = 0;
+    private ILine line;
+
+    public LineDrawerAdapter() {
+        super();
+        this.line = LineFactory.getBasicLine();
+    }
+    public LineDrawerAdapter(ILine line) {
+        super();
+        this.line = line;
+    }
+    public void setLine(ILine line) {
+        this.line = line;
+    }
 
     @Override public void setPosition(int i, int i1) {
         this.startX = i;
@@ -21,5 +35,9 @@ public class LineDrawerAdapter implements Job2dDriver {
         line.setEndCoordinates(i, i1);
         setPosition(i, i1);
         DrawerFeature.getDrawerController().drawLine(line);
+    }
+
+    @Override public String toString() {
+        return "LineDrawerAdapter{" + "startX=" + startX + ", startY=" + startY + ", line=" + line + '}';
     }
 }
