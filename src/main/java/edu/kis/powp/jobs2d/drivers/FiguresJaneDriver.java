70b985664625b806328ba6a231c57535574df2fe
209515 <209515@edu.p.lodz.pl>
2019-05-07 12:10:48 +0200

    3.4.1 Dodano klasę FiguresJaneDriver, której konstrukcja przypomina klasę LineDrawerAdapter lecz wykorzystuje metody i pola klasy AbstractDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/FiguresJaneDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/FiguresJaneDriver.java
@@ -1,17 +1,32 @@
 package edu.kis.powp.jobs2d.drivers;
 
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 public class FiguresJaneDriver extends AbstractDriver{
+	private ILine line;
+	
+	public FiguresJaneDriver(int _x, int _y, ILine _line) {
+		super(_x, _y);
+		this.line = _line;
+	}
 
-	public FiguresJaneDriver(int x, int y) {
-		super(x, y);
+
+	@Override
+	public void operateTo(int _x, int _y) {
+		ILine line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.getX(), this.getY());
+		line.setEndCoordinates(_x, _y);
+		this.setPosition(_x, _y);
+
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
-	public void operateTo(int x, int y) {
-		// TODO Auto-generated method stub
-		
+	public String toString() {
+		return "@FJ!$!@$!#@$(*#@&Q(%^*#@";
 	}
 
 }
