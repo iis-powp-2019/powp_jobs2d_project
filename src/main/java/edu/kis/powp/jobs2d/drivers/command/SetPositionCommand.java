028564d1bcb36009bf4316aff596e6977dae296c
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-05-08 15:07:06 +0200

    3.1.1 new structure from diagram implemented

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+
+    @Override public void execute() {
+
+    }
+
+
+}
