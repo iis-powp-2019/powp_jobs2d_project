ff75c9edbf052b2b668dd1df39e45937f8ed872c
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-05-08 15:05:08 +0200

    C3.1.3 add class ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,19 @@
+package edu.kis.powp.command;
+
+import java.util.List;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ComplexCommand implements DriverCommand {
+	private List<DriverCommand> listOfCommand;
+
+	public ComplexCommand(List<DriverCommand> list) {
+		listOfCommand = list;
+	}
+
+	@Override 
+	public void execute(Job2dDriver job2dDriver) {
+		for (DriverCommand item : listOfCommand)
+			item.execute(job2dDriver);
+	}
+}
