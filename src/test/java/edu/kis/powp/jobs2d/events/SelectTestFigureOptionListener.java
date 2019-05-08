0cfe7bf231099e9019010b45c08c5aac8abb54c6
209278 <209278@edu.p.lodz.pl>
2019-05-08 15:04:29 +0200

    3.1.2 Wykorzystanie DriverCommand do narysowanie lini

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.util.function.Consumer;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.command.OperateToCommand;
+import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
@@ -20,6 +22,12 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		consumer.accept(driverManager.getCurrentDriver());
+//		consumer.accept(driverManager.getCurrentDriver());
+		OperateToCommand operateToCommand = new OperateToCommand(driverManager.getCurrentDriver());
+		SetPositionCommand setPositionCommand = new SetPositionCommand(driverManager.getCurrentDriver());
+
+		setPositionCommand.execute(100, 100);
+		operateToCommand.execute(-100, -100);
+
 	}
 }
