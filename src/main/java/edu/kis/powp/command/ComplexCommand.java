b1084bca3dda3069dff3f5318b72278e0066e769
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2019-05-13 07:40:57 +0200

    3.1.3 Add Job2dDriver to execute method and to Interface

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,23 +1,25 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
     List<DriverCommand> commandList;
 
-    public ComplexCommand(List<DriverCommand> commandList) {
-        this.commandList = commandList;
-    }
-
     public ComplexCommand(DriverCommand... commands) {
         this(Arrays.asList(commands));
     }
 
+    public ComplexCommand(List<DriverCommand> commandList) {
+        this.commandList = commandList;
+    }
+
     @Override
-    public void execute() {
+    public void execute(Job2dDriver job2dDriver) {
         for(DriverCommand command: commandList) {
-            command.execute();
+            command.execute(job2dDriver);
         }
     }
 }
