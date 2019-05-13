1668ee704c96a1cb8271e2acd4c453aa0c6657da
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2019-05-13 07:36:13 +0200

    3.1.3 (WIP) Create commandList to draw any shape

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,9 +1,23 @@
 package edu.kis.powp.command;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ComplexCommand implements DriverCommand{
+    List<DriverCommand> commandList;
+
+    public ComplexCommand(List<DriverCommand> commandList) {
+        this.commandList = commandList;
+    }
+
+    public ComplexCommand(DriverCommand... commands) {
+        this(Arrays.asList(commands));
+    }
 
     @Override
     public void execute() {
-        
+        for(DriverCommand command: commandList) {
+            command.execute();
+        }
     }
 }
