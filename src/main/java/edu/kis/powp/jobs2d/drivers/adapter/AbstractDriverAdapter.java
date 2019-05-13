2db4514df448fe4924a488b3f2385202fb0e8459
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-05-13 12:25:10 +0200

    code clean up

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -4,7 +4,6 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.AbstractDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 public class AbstractDriverAdapter extends AbstractDriver {
     private int startX = 0, startY = 0;
