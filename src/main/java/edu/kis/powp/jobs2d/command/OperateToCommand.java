17a11289771761b2568b961a5abeadb412eb8efa
Eccleastic <eccleastic@gmail.com>
2019-05-15 14:54:57 +0200

    3.1 Poprawienie struktury wzorca Command

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -1,11 +1,20 @@
 package edu.kis.powp.jobs2d.command;
 
-public class OperateToCommand implements DriverCommand{
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
 
     int x;
     int y;
+    private Job2dDriver job2dDriver;
+
+    public OperateToCommand(int x, int y, Job2dDriver job2dDriver) {
+        this.x = x;
+        this.y = y;
+        this.job2dDriver = job2dDriver;
+    }
 
     @Override public void execute() {
-
+        job2dDriver.operateTo(this.x, this.y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,11 +1,20 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 
     int x;
     int y;
+    private Job2dDriver job2dDriver;
+
+    public SetPositionCommand(int x, int y, Job2dDriver job2dDriver) {
+        this.x = x;
+        this.y = y;
+        this.job2dDriver = job2dDriver;
+    }
 
     @Override public void execute() {
-
+        job2dDriver.setPosition(this.x, this.y);
     }
 }
