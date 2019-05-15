b0101610d7c88385b0fb464d29a71a5e89879061
nazariitkachuk <tknazarko@gmail.com>
2019-05-15 18:36:14 +0200

    Added new lineParameters.class

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/lineParameters.java
@@ -0,0 +1,30 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import java.awt.Color;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.line.AbstractLine;
+
+public class lineParameters extends AbstractLine implements ILine{
+	public lineParameters() {
+		super();
+		color = Color.black;
+		thickness = 3.0f;
+		dotted = false;
+	}
+	
+	public lineParameters(Color color,float thickness,boolean dotted) {
+		super();
+		this.color = Color.black;
+		this.thickness = 3.0f;
+		this.dotted = false;
+	}
+	
+	public lineParameters(ILine line) {
+        super();
+        this.color = line.getColor();
+        this.thickness = line.getThickness();
+        this.dotted = line.isDotted();
+    }
+	
+}
