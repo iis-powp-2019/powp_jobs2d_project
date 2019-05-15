013fff7d62cbbe4dca9e58932816b33adb7ea849
Delicja05 <209296@edu.p.lodz.pl>
2019-05-15 15:39:18 +0300

    poprawki

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
@@ -8,12 +8,11 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
+public class Job2dAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;	
 	private DrawPanelController drawPanelController;
 
 	public Job2dAdapter(DrawPanelController drawPanelController) {
-		super();
 		this.drawPanelController = drawPanelController;
 	}
 
