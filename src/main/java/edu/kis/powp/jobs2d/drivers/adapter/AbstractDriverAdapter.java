e52504598bac452841403b49971d6b3cdbb03a64
Rootar <mokrosinski95@gmail.com>
2019-05-15 05:50:00 +0200

    3.4.1 Dodanie nowego Adaptera na figury wykorzystujące abstract driver

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -0,0 +1,39 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+import edu.kis.powp.jobs2d.features.DriverFeature;
+
+/**
+ * driver adapter to drawer with several bugs.
+ */
+public class AbstractDriverAdapter extends AbstractDriver {
+//	private int x = 0, y = 0;
+
+	public AbstractDriverAdapter() {
+		super(0, 0);
+	}
+
+
+	/**
+	 * Moves the head (in active mode) to the given coordinates.
+	 *
+	 * @param x end of the line in X axis.
+	 * @param y end of the line in Y axis.
+	 */
+	@Override
+	public void operateTo(int x, int y) {
+		DriverFeature.getDriverManager().getCurrentDriver().setPosition(this.getX(), this.getY());
+		DriverFeature.getDriverManager().getCurrentDriver().operateTo(x, y);
+		this.setPosition(x, y);
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractDriverAdapter";
+	}
+}
