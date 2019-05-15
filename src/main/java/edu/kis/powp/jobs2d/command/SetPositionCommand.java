d871ccb3d1ba23775d6f03198c9e61700cd97609
Rootar <mokrosinski95@gmail.com>
2019-05-15 15:18:19 +0200

    3.1.1 Zaprojektowanie hierarchi klas

---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+
+public class SetPositionCommand implements DriverCommand {
+    int x, y;
+    public Job2dDriver job2dDriver;
+
+    public SetPositionCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.job2dDriver = job2dDriver;
+        execute();
+    }
+
+    @Override
+    public void execute() {
+        job2dDriver.setPosition(x, y);
+    }
+}
