34de2e67b9e8fbc3c3f8b0608caa5884a7d1fd1b
Marcin Jeznach <scheissig@gmail.com>
2019-05-15 15:17:23 +0200

    Lab Command Wykonano punkt 3.1.2 - implementacja hierarchii klas command

--- src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
@@ -1,6 +1,11 @@
 package edu.kis.powp.jobs2d.command;
 
+import com.sun.istack.internal.NotNull;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public interface DriverCommand {
 
+    void setDriver(Job2dDriver driver);
     void execute();
+    void execute(@NotNull Job2dDriver driver);
 }
--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,11 +1,36 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    SetPositionCommand(int x, int y, Job2dDriver driver) {
+        this(x, y);
+        this.driver = driver;
+    }
+
+    @Override
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
 
     @Override
     public void execute() {
-        // TODO implementation
+        if (this.driver == null) throw new NullPointerException("No driver provided");
+        this.driver.setPosition(this.x, this.y);
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        this.setDriver(driver);
+        this.execute();
     }
 }
