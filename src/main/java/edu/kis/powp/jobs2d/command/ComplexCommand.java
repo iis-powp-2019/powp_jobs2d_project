7ef2e1000952b760a89fbd0ab6e35deb66ea3dc9
Eccleastic <eccleastic@gmail.com>
2019-05-15 15:11:54 +0200

    3.3 Implementacja klasy ComplexCommand

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+    private List<DriverCommand> driverCommands;
+
+    public ComplexCommand(List<DriverCommand> driverCommands) {
+        this.driverCommands = driverCommands;
+    }
+
+    public void addCommand(DriverCommand driverCommand){
+        driverCommands.add(driverCommand);
+    }
+
+    @Override public void execute() {
+        for(DriverCommand driverCommand : driverCommands){
+            execute();
+        }
+    }
+}
