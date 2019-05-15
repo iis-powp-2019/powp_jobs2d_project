20a0884a7ba36ba33719011de476957247c4a47d
Szymon Sobieszczyk <esesinho@gmail.com>
2019-05-15 14:49:18 +0200

    3.1.2 Implementacja klas

--- src/main/java/edu/kis/powp/jobs2d/drivers/Command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/Command/SetPositionCommand.java
@@ -1,10 +1,19 @@
 package edu.kis.powp.jobs2d.drivers.Command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand{
     private int x;
     private int y;
+    private Job2dDriver driver;
 
+    SetPositionCommand(int x, int y,  Job2dDriver driver){
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+    @Override
      public void execute() {
-
+        driver.setPosition(this.x,this.y);
     }
 }
