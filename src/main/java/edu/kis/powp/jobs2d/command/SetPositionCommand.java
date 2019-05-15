ad153f1e236c42c3cf00b65def6d70ce3e5e0c27
Krystian Nowak <202247@edu.p.lodz.pl>
2019-05-15 15:26:06 +0200

    Lab4_3.1.2 Add constructors & refactor execute method

--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,16 +1,25 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 
 	private int x;
 	private int y;
 	
 	
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
+	public SetPositionCommand(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
 	}
+
+
+	@Override
+	public void execute(Job2dDriver driver) {
+		driver.setPosition(x, y);
+	}
+
 	
 	
 }
