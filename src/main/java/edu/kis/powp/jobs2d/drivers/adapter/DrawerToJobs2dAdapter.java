20211287dfd4d7594bcd715d9d3168eaef3ee7c9
Rootar <mokrosinski95@gmail.com>
2019-05-15 04:10:46 +0200

    3.2.1 Naprawienie adaptera oraz zintegrowanie okna rysowania z oknem aplikacji

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerToJobs2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerToJobs2dAdapter.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -36,14 +37,18 @@ public class DrawerToJobs2dAdapter extends DrawPanelController implements Job2dD
 	@Override
 	public void operateTo(int x, int y) {
 		ILine line = LineFactory.getBasicLine();
+
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		this.startX = x;
+		this.startY = y;
+
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "DrawerToJobs2dAdapter";
 	}
 }
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -40,7 +40,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
 		Job2dDriver testDriver = new DrawerToJobs2dAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		DriverFeature.addDriver("Graphics Driver", testDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
@@ -52,8 +52,10 @@ public class TestJobs2dPatterns {
 	 */
 	private static void setupDefaultDrawerVisibilityManagement(Application application) {
 		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
+
 		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
 				new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
+
 		defaultDrawerWindow.setVisible(true);
 	}
 
@@ -83,7 +85,7 @@ public class TestJobs2dPatterns {
 			public void run() {
 				Application app = new Application("2d jobs Visio");
 				DrawerFeature.setupDrawerPlugin(app);
-				setupDefaultDrawerVisibilityManagement(app);
+//				setupDefaultDrawerVisibilityManagement(app);
 
 				DriverFeature.setupDriverPlugin(app);
 				setupDrivers(app);
