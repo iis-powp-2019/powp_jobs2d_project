5695a194ef82d864c8dcdac04aff2a80ff712b8d
mkarpacka <209348@edu.p.lodz.pl>
2019-05-15 14:58:18 +0200

    3.1 dodana implementacja klas

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -1,12 +1,22 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand{
 
-	int x, y;
+	private int x, y;
+	private Job2dDriver driver;
+	
+	public OperateToCommand(Job2dDriver driver, int x, int y){
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+
 	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		driver.operateTo(x, y);
 		
 	}
 
--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,12 +1,21 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand{
 	
-	int x, y;
+	private int x, y;
+	private Job2dDriver driver;
+	
+	public SetPositionCommand(Job2dDriver driver, int x, int y){
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		driver.setPosition(x, y);
 		
 	}
 
