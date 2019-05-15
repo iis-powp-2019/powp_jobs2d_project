55eb009fb41aab918aa55f6db2cebcf3d4d7ec16
Wojciech Oset <209415@edu.p.lodz.pl>
2019-05-15 14:39:19 +0200

    3.1.2

--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,7 +1,16 @@
 package edu.kis.powp.jobs2d.command;
+import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class SetPositionCommand implements DriverCommand {
+    private int x, y;
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
-    public void execute() {
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.setPosition(x, y);
     }
 }
