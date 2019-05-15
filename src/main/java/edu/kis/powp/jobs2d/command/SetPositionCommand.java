d89bc671d7d9dd65ec9e0245abf89a7ec820dba2
Delicja05 <209296@edu.p.lodz.pl>
2019-05-15 15:53:07 +0300

    3.1.1. - hierachia klas wedlug diagramu

---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand{
+	
+	private Job2dDriver job2dDriver;
+
+    public SetPositionCommand(Job2dDriver job2dDriver){
+        this.job2dDriver = job2dDriver;
+    }
+
+    @Override
+    public void execute(int x, int y) {
+        job2dDriver.setPosition(x, y);
+    }
+
+}
