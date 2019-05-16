ff7afc3fcdb1f267eaef70f526e6920eb7769c66
209450 <209450@edu.p.lodz.pl>
2019-05-16 13:40:26 +0200

    Adapter z wyborem rodzaju linii

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,25 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+public class LineDrawerAdapter extends DrawPanelControllerAdapter {
+
+    private ILine iLine;
+
+    public LineDrawerAdapter(ILine iLine) {
+        this.iLine = iLine;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        iLine.setStartCoordinates(this.startX, this.startY);
+        iLine.setEndCoordinates(x, y);
+
+        DrawerFeature.getDrawerController().drawLine(iLine);
+        setPosition(x,y);
+    }
+}
