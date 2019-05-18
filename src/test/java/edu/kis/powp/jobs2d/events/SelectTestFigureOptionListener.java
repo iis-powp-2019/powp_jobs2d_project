a6044babe01915f56a4767712495c64ece134b4f
Marcin Jeznach <scheissig@gmail.com>
2019-05-18 15:26:44 +0200

    Lab Adapter Wykonano punkt 3.2.3 - dodanie testu dla figureScript2 (refaktoryzacja testu - wzorzec rysunku jako parametr)

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,19 +3,25 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
-import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+	@FunctionalInterface
+	public interface FigureScript {
+		void draw(Job2dDriver driverManager);
+	}
+	private FigureScript figure;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, FigureScript figure) {
 		this.driverManager = driverManager;
+		this.figure = figure;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		figure.draw(driverManager.getCurrentDriver());
 	}
 }
