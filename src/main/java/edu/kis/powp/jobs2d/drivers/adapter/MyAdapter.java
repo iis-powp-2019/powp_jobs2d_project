0264a7ef7ac88a27971420539730a75401d61112
Marcin Jeznach <scheissig@gmail.com>
2019-05-18 11:25:33 +0200

    Revert "Wykonano punkt 3.2.0 - naprawa funkcji operateTo" Ponowne podejśćie do laboratorium z adapterem
    This reverts commit e7e1b254dd0080b8d9ce8f0a91130befc7ab564b.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -26,7 +26,6 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		setPosition(x, y);
 
 		drawLine(line);
 	}
