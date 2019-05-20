26f92eb73af48f43f141d89e95c89efcce4a89ae
hadi97 <hadielwazer97@gmail.com>
2019-05-20 22:29:46 +0200

    projektowanie hierarchię command

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -0,0 +1,19 @@
+package edu.kis.powp.jobs2d.commands;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.setPosition(x,y);
+    }
+}
