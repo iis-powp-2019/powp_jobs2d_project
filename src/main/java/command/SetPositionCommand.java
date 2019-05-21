bfb548968f5090f80ea11a96e654ebba60cd4c95
nazariitkachuk <tknazarko@gmail.com>
2019-05-21 20:00:49 +0200

    Implemented  class hierarhy

---
+++ src/main/java/command/SetPositionCommand.java
@@ -0,0 +1,20 @@
+package command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+	private int x,y;
+    private Job2dDriver driver;
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.driver = driver;
+    }
+
+    @Override
+    public void execute() {
+        driver.setPosition(x, y);
+    }
+
+}
