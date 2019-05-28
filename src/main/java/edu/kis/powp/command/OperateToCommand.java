d3ff6369f13c4fa88abd14932b0184c93495bda0
Mikołaj Baran <tarralan@wp.pl>
2019-05-29 00:29:10 +0200

    3.3.2 Dopasowanie hiererhi polecen

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,10 +1,12 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
     int x;
     int y;
     @Override
-    public void execute() {
-
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(x,y);
     }
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -1,10 +1,17 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
     int x;
     int y;
     @Override
-    public void execute() {
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(this.x,this.y);
+    }
 
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
