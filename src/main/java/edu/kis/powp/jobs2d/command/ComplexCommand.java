401c8096fa86e951e0c3ade3613487e0c428b9ec
Piotr Rogowski <polishotaku@gmail.com>
2019-05-29 23:07:12 +0200

    Added ComplexComand

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,32 @@
+package edu.kis.powp.jobs2d.command;
+
+
+
+import javafx.util.Pair;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ComplexCommand{
+    private Map<DriverCommand, Pair<Integer, Integer>> commands;
+
+    public ComplexCommand(Map<DriverCommand, Pair<Integer, Integer>> commands){
+        this.commands = commands;
+    }
+
+    public ComplexCommand(){
+        commands = new LinkedHashMap<>();
+    }
+
+
+    public void execute() {
+        for (DriverCommand temp:
+             commands.keySet()) {
+            temp.execute(commands.get(temp).getKey(), commands.get(temp).getValue());
+        }
+    }
+
+    public void addCommand(DriverCommand driverCommand, Pair<Integer, Integer> integerPair){
+        commands.put(driverCommand, integerPair);
+    }
+}
