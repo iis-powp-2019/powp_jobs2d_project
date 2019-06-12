bef11cebe4aae8b812a3af325ea02529c18f511c
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-06-12 11:30:28 +0200

    Lab 4 ComplexCommand + test

--- src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
@@ -5,6 +5,6 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 public interface DriverCommand {
 
-    void execute(Job2dDriver driver);
+    void execute();
 
 }
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -11,6 +11,7 @@ import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawPanelControllerToJobs2dDriverAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.SpecialLineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
+import edu.kis.powp.jobs2d.events.SelectTestCustomFigureOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener2;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -31,8 +32,12 @@ public class TestJobs2dPatterns {
 		SelectTestFigureOptionListener2 selectTestFigureOptionListener2 = new SelectTestFigureOptionListener2(
 				DriverFeature.getDriverManager());
 
+		SelectTestCustomFigureOptionListener selectTestCustomFigureOptionListener =
+				new SelectTestCustomFigureOptionListener(DriverFeature.getDriverManager());
+
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
 		application.addTest("Figure Joe 2", selectTestFigureOptionListener2);
+		application.addTest("Custom", selectTestCustomFigureOptionListener);
 	}
 
 	/**
